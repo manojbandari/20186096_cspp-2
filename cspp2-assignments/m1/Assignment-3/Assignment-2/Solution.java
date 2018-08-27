548b0c03144a8cde9643c260cd3a960593a6f049
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-27 22:31:55 +0530

    area of triangle

---
+++ cspp2-assignments/m1/Assignment-3/Assignment-2/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int base = scan.nextInt();
+		int height = scan.nextInt();
+		System.out.print(areaOfTriangle(base, height));
+
+	}
+	/*
+	Need to fill the areaOfTriangle function and print the output
+	of fahrenheit.
+	*/
+	public static double areaOfTriangle(int base,int height){
+		return 0.5*base*height;
+	}
+}
