f6df77d92b1dc460107806b9c987a6dff294fad4
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-27 22:03:17 +0530

    Quotient_remainder

---
+++ cspp2-practice/m1/quotient_remainder.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class quotient_remainder{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("a=");
+		int a = s. nextInt();
+		System.out.print("b=");
+		int b = s. nextInt();
+		System.out.println("Remainder ="+(a%b)); 
+		System.out.println("Quotient ="+(a/b)); 
+
+	}
+}
