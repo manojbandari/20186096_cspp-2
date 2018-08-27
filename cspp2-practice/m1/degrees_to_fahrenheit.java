f529bb3e08efbfc393a836dfca1633cef7e91212
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-27 22:48:09 +0530

    degree to Farenheit

---
+++ cspp2-practice/m1/degrees_to_fahrenheit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class degrees_to_fahrenheit{
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double degreesCelsius = scan.nextDouble();
+		System.out.println(calculateFromDegreesToFarenheit(degreesCelsius));
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+	public static double calculateFromDegreesToFarenheit(double degreesCelsius){
+		return  degreesCelsius*1.8 + 32;
+	}
+}
