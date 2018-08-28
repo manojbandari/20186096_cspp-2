7c63f26b8fb9f1be514643cdfff6b1966a0ad3bc
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-28 19:53:08 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Assignment-2/Solution.java
@@ -9,7 +9,7 @@ public final class Solution {
      * Constructs the object.
      */
     private Solution() {
-    // we are not using this constructor.   
+    // we are not using this constructor.
     }
     /**
     *Do not modify this main function.
@@ -23,15 +23,15 @@ public final class Solution {
         rootsOfQuadraticEquation(a, b, c);
     }
     /**
-    *Need to write the rootsOfQuadraticEquation function 
+    *Need to write the rootsOfQuadraticEquation function.
     *and print the output.
     *@param      a     { parameter_description }
     *@param      b     { parameter_description }
     *@param      c     { parameter_description }
     */
     public static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
-        double firstroot = ( - b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
-        double secondroot = ( - b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+        double firstroot = ( -b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+        double secondroot = ( -b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
         System.out.println(firstroot + " " + secondroot);
     }
 }
