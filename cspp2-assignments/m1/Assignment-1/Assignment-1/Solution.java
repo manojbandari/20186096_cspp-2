a2bd3b08e7fa19972bd9d043739aaeccf172f8f4
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-28 17:25:05 +0530

    name and roll no

--- cspp2-assignments/m1/Assignment-1/Assignment-1/Solution.java
+++ cspp2-assignments/m1/Assignment-1/Assignment-1/Solution.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
-		System.out.println("Name : , Roll Number : ");
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter name and roll number:");
+		String name = s.nextLine();
+		int roll= s.nextInt();
+		System.out.println("Name :"+name +"   "+"Roll Number : "+roll);
 	}
 }
--- cspp2-assignments/m2/Assignment-3/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Assignment-3/Solution.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Solution {
 	/*
@@ -8,10 +9,20 @@ public class Solution {
 		Scanner s=new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-        int result=power(base,exponent);
+        long result=power(base,exponent);
         System.out.println(result);
 	}
 	/*
 	Need to write the power function and print the output.
 	*/
+	public static long power(int base,int exponent){
+		//return Math.pow(base,exponent);
+		if(exponent != 0){
+			return base*(power(base, exponent-1));
+		}
+		else{
+			return 1;
+		}
+		
+	}
 }
