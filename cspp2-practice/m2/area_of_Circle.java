742cc5f79dfcec61607782f5d005db164f51563d
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-28 14:24:40 +0530

    area of circle

--- cspp2-practice/m2/area_of_Circle.java
+++ cspp2-practice/m2/area_of_Circle.java
@@ -3,7 +3,14 @@ class area_of_Circle{
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.print("enter radius: ");
-		float r = s.nextInt();
-		System.out.println(3.14*r*r);
+		int r = s.nextInt();
+		System.out.println(area(r));
 	}
+	public static double area(int r){
+		return pi()*r*r;
+	}
+	public static double pi(){
+		return 3.14;
+	}
+
 }
