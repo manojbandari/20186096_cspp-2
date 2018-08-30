34df91db3602973fd4fbda53746f4e7915d7e9a6
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-30 14:23:07 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 /**
  * @author:manojbandari
  */
-public final class Solution
-{   
+public final class Solution {
     /**
      * Constructs the object.
      */
@@ -18,7 +17,7 @@ public final class Solution
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int a[] = new int[n];
+        int []a = new int[n];
         int max = 0;
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
@@ -30,3 +29,4 @@ public final class Solution
 
     }
 }
+
