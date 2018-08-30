31c11c488b3d5d60383f46ca3132ea3c9d900776
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-30 19:50:41 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -7,32 +7,31 @@ public class Solution {
 		int c1 = s.nextInt();
 		int[][] a1 = new int[r1][c1];
 		int[][] a2 = new int[r1][c1];
-		for(int i = 0; i < r1; i++) {
-			for(int j = 0; j < c1; j++) {
-			a1[i][j] = s.nextInt();
+		for (int i = 0; i < r1; i++) {
+			for (int j = 0; j < c1; j++) {
+				a1[i][j] = s.nextInt();
 			}
 		}
 		int r2 = s.nextInt();
 		int c2 = s.nextInt();
-		for(int i = 0; i < r2; i++) {
-			for(int j = 0; j < c2; j++) {
+		for (int i = 0; i < r2; i++) {
+			for (int j = 0; j < c2; j++) {
 				a2[i][j] = s.nextInt();
 			}
 		}
-		if (r1 == r2 && c1 == c2){
+		if (r1 == r2 && c1 == c2) {
 			int[][] sum = new int[r1][c1];
-			for(int i = 0; i < r1; i++) {
-				for(int j = 0;j < c1; j++) {
+			for (int i = 0; i < r1; i++) {
+				for (int j = 0; j < c1; j++) {
 					sum[i][j] = a1[i][j] + a2[i][j];
-				}	
+				}
 			}
-			for(int i = 0; i < r1 ; i++) {
-				for(int j = 0; j < c1; j++) {
-					if(j == c1-1){
-						System.out.print(sum[i][j]);					
-					}
-					else{
-						System.out.print(sum[i][j]+" ");
+			for (int i = 0; i < r1 ; i++) {
+				for (int j = 0; j < c1; j++) {
+					if (j == c1 - 1) {
+						System.out.print(sum[i][j]);
+					} else {
+						System.out.print(sum[i][j] + " ");
 					}
 				}
 				System.out.println("");
@@ -42,6 +41,6 @@ public class Solution {
 		else {
 			System.out.println("not possible");
 		}
-	
+
 	}
 }
