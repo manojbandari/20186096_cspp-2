28e30f71523bf4853d71c6ba39adb4521eb0c380
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-30 13:52:18 +0530

    Average

---
+++ cspp2-practice/m4/FirstLast6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class FirstLast6 {
+	public static int N_temp = 6;
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int a[]= new int[n];
+		for (int i=0;i<n;i++) {
+			a[i]=s.nextInt();
+		}
+		if (a[0]==N_temp)
+			System.out.println(true);
+		else if (a[a.length-1]==N_temp)
+			System.out.println(true);
+		else
+			System.out.println(false);
+	}
+}
