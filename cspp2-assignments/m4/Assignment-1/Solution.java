5c5eab711fcf70b2b5cad78a9d10304c4a646aa5
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-30 14:18:25 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -17,14 +17,15 @@ public class Solution
 	*/
 	public static void main(final String[] args)
 	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		int a[]= new int[n];
-		int max=0;
-		for (int i=0;i<n;i++) {
-			a[i]=sc.nextInt();
-			if (max < a[i])
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		int max = 0;
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+			if (max < a[i]) {
 				max = a[i];
+			}
 		}
 		System.out.println(max);
 
