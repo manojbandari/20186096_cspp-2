ec14c6891d43e8bcdd89bb16d0e5b52a234b6abc
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-05 09:59:42 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -1,43 +1,53 @@
-/** author : manojbandari
-*program to check the valid data
+/** author : manojbandari.
+*program to check the valid data.
 */
-import java.util.*;
-class InputValidator {	
-	/**
-	 * { flag variable }
-	 */
-	static int flag = 0;
-	
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      input  The input
-	 */
-	InputValidator(String input) {
-		if (input.length() > 5) {
-			flag = 1;
-		}
+import java.util.Scanner;
+/**
+ * Class for input validator.
+ */
+class InputValidator {
+    /**
+     * { flag variable }.
+     */
+    private static int flag = 0;
+    public static int TEMP = 5;
+    /**
+     * Constructs the object.
+     *
+     * @param      input  The input
+     */
+    InputValidator(final String input) {
+        if (input.length() > TEMP) {
+            flag = 1;
+        }
 
-	}
-	/**
-	 * { to validate data }
-	 *
-	 * @return     { return_value is boolean }
-	 */
-	public static boolean validateData() {
-		if (flag == 1) {
-			return true;
-		}
-		return false;
-	}
+    }
+    /**
+     * { to validate data }.
+     *
+     * @return     { return_value is boolean }.
+     */
+    public static boolean validateData() {
+        if (flag == 1) {
+            return true;
+        }
+        return false;
+    }
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String args[]) {
-		Scanner s = new Scanner(System.in);
-		String input = s.next();
-		InputValidator i = new InputValidator(input);
-		System.out.println(i.validateData());
 
-	}
+    private Solution() {
 
+    }
+
+    public static void main(final String args[]) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
+
+    }
 }
