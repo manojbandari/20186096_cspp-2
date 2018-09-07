5d6fa05f5fd5ac33abe211e96526eb826dee318a
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-07 10:44:03 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-1/List.java
+++ cspp2-assignments/m11/Assignment-1/List.java
@@ -228,21 +228,26 @@ public class List {
 	public List subList(int start, int end) {
 		// write the logic for subList
 		List sublist = new List();
-		if (start >= 0 && end < size) {
-			if (start < end) {
-				for (int i = start; i < end; i++) {
-					sublist.add(list[i]);
+		if (start >= 0 && start < size) {
+			if(end >=0 && end < size) {
+				if (start < end) {
+					for (int i = start; i < end; i++) {
+						sublist.add(list[i]);
+					}
+					return sublist;
+				} else if (start == end) {
+					return sublist;
+				} else {
+					System.out.println("Index Out of Bounds Exception");
+					return null;
 				}
-				return sublist;
-			} else if (start == end) {
-				return sublist;
 			} else {
 				System.out.println("Invalid Position Exception");
 				return null;
 			}
 		} else {
 			System.out.println("Invalid Position Exception");
-				return null;
+			return null;
 		}
 	}
 	/*
