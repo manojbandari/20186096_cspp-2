e042c2e79c83352d26efc16f3f1367020ef47e61
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-11 00:08:47 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m14/SortedSet/SortedSet.java
+++ cspp2-assignments/m14/SortedSet/SortedSet.java
@@ -57,7 +57,7 @@ public class SortedSet extends Set {
 	}
 	public Set subSet(int fromElement, int toElement){
 		SortedSet subset= new SortedSet();
-		if((size()==1 && set[0]==toElement)||get(toElement)==-1) {
+		if(size()==1 && set[0]==toElement) {
 			return subset;
 		}
 		int start= indexOf(fromElement);
