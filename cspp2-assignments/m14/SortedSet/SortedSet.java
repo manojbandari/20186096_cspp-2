b1498e57a983178ae5527f546992ad9781aca578
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-10 19:48:50 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m14/SortedSet/SortedSet.java
+++ cspp2-assignments/m14/SortedSet/SortedSet.java
@@ -18,7 +18,7 @@ public class SortedSet extends Set {
             	int k;
             	for(k=0;k<size();k++) {
             		if(value < get(k)) {
-            			for(int z=size()-1;z<k;z--)
+            			for(int z=size()-1;z>k;z--)
             				set[z+1]=set[z];
 
             			set[k]=value;
