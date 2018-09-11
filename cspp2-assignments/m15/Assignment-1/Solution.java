822feeadd75a88808dcc8e25ec6cda528adbef36
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-11 17:34:36 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-1/Solution.java
+++ cspp2-assignments/m15/Assignment-1/Solution.java
@@ -55,7 +55,7 @@ class Solution {
 	}
 	public void remove(int index) throws InvalidPositionException {
 		/*int m = indexOf(element);*/
-		if(index<0 || index>=size) {
+		if(index<0 || index>size) {
 			throw new InvalidPositionException();
 		}
 		for (int i = index; i < size; i++) {
