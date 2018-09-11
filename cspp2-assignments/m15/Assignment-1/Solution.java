b91a2c6bc1cabf72ec70a666784fc4234a6e0a55
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-11 15:00:33 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-1/Solution.java
+++ cspp2-assignments/m15/Assignment-1/Solution.java
@@ -13,68 +13,67 @@ class Solution {
 		size = 0;
 	}
 	public void add(int element) {
-		if(size==list.length) {
+		if (size == list.length) {
 			resize();
 		}
-		list[size++]=element;
+		list[size++] = element;
 	}
-	public void add(int index,int element) {
+	public void add(int index, int element) {
 		int i;
-		for(i=size-1;i>index;i++) {
-			list[i+1]=list[i];
+		for (i = size - 1; i > index; i++) {
+			list[i + 1] = list[i];
 		}
-		list[i]=element;
+		list[i] = element;
 		size++;
 	}
 	public void resize() {
-		list=Arrays.copyOf(list,size*2);
+		list = Arrays.copyOf(list, size * 2);
 	}
 	public int size() {
 		return size;
 	}
 	public void clear() {
-		size=0;
-		list=new int[10];
+		size = 0;
+		list = new int[10];
 	}
 	public boolean contains(int element) {
-		return indexOf(element)!=-1;
+		return indexOf(element) != -1;
 	}
 	public int get(int index) {
-		if(index<0 || index> size) {
+		if (index < 0 || index > size) {
 			return -1;
 		}
 		return list[index];
 	}
 	public int indexOf(int element) {
-		for(int i=0;i<size;i++) {
-			if(list[i]==element) {
+		for (int i = 0; i < size; i++) {
+			if (list[i] == element) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	public void remove(int element){
-		int m=indexOf(element);
-		for(int i=m;i<size;i++)
-		{
-			list[i]=list[i+1];
+	public void remove(int element) {
+		int m = indexOf(element);
+		for (int i = m; i < size; i++) {
+			list[i] = list[i + 1];
 		}
 		size--;
 	}
 	public void addAll(int[] arr) {
-		for(int i=0;i<arr.length;i++) {
+		for (int i = 0; i < arr.length; i++) {
 			add(arr[i]);
 		}
 	}
 	public void removeAll(int[] arr) {
-		for(int i=0;i<arr.length;i++) {
+		for (int i = 0; i < arr.length; i++) {
 			remove(arr[i]);
 		}
 	}
 	public int count(int element) {
-		int count=0;
-		for(int i=0;i<size;i++) {
-			if(list[i]==element) {
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			if (list[i] == element) {
 				count++;
 			}
 		}
@@ -84,115 +83,112 @@ class Solution {
 		return this.toString().equals(other.toString());
 	}
 	public String toString() {
-		if(size==0) {
+		if (size == 0) {
 			return "[]";
 		}
-		String str="[";
-		int i=0;
-		for( i=0;i<size-1;i++) {
-			str=str+list[i]+",";
+		String str = "[";
+		int i = 0;
+		for ( i = 0; i < size - 1; i++) {
+			str = str + list[i] + ",";
 		}
-		str=str+list[i]+"]";
+		str = str + list[i] + "]";
 		return str;
 	}
 	public Solution subList(int start, int end) {
-		Solution newlist= new Solution();
-		try {
-			if(start<0 || end >=size);
-		}
-		catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("ArrayIndexOutOfBoundsException");
-		}
-
-		for(int i=start;i<end;i++) {
+		Solution newlist = new Solution();
+		for (int i = start; i < end; i++) {
 			newlist.add(list[i]);
 		}
 		return newlist;
 	}
-	public static void main(String[] args) throws InvalidPositionException{
-		Solution l=new Solution();
+	public static void main(String[] args) throws InvalidPositionException {
+		Solution l = new Solution();
 		Scanner stdin = new Scanner(new BufferedInputStream(System.in));
-		while(stdin.hasNext()) {
+		while (stdin.hasNext()) {
 			String line = stdin.nextLine();
-			String[] tokens=line.split(" ");
-			switch(tokens[0]) {
-				case "add":
-					String[] a = tokens[1].split(",");
-					if(a.length==2) {
-						l.add(Integer.parseInt(a[0]),Integer.parseInt(a[1]));
-					} else if(a.length==1){
-							l.add(Integer.parseInt(tokens[1]));
+			String[] tokens = line.split(" ");
+			switch (tokens[0]) {
+			case "add":
+				String[] a = tokens[1].split(",");
+				if (a.length == 2) {
+					l.add(Integer.parseInt(a[0]), Integer.parseInt(a[1]));
+				} else if (a.length == 1) {
+					l.add(Integer.parseInt(tokens[1]));
+				}
+				break;
+			case "size":
+				System.out.println(l.size());
+				break;
+			case "remove":
+				if (tokens.length == 2) {
+					l.remove(Integer.parseInt(tokens[1]));
+					throw new InvalidPositionException("InvalidPositionException");
+				}
+				break;
+			case "get":
+				System.out.println(l.get(Integer.parseInt(tokens[1])));
+				break;
+			case "contains":
+				if (tokens.length == 2) {
+					System.out.println(l.contains(Integer.parseInt(tokens[1])));
+				}
+				break;
+			case "indexOf":
+				if (tokens.length == 2) {
+					System.out.println(l.indexOf(Integer.parseInt(tokens[1])));
+				}
+				break;
+			case "addAll":
+				if (tokens.length == 2) {
+					String[] b = tokens[1].split(",");
+					int[] temp = new int[b.length];
+					for (int i = 0; i < b.length; i++) {
+						temp[i] = Integer.parseInt(b[i]);
 					}
-					break;
-				case "size":
-					System.out.println(l.size());
-					break;
-				case "remove":
-						if(tokens.length==2) {
-							l.remove(Integer.parseInt(tokens[1]));
-							throw new InvalidPositionException("InvalidPositionException");
-						}
-					break;
-				case "get":
-					System.out.println(l.get(Integer.parseInt(tokens[1])));
-					break;
-				case "contains":
-					if(tokens.length==2) {
-						System.out.println(l.contains(Integer.parseInt(tokens[1])));
+					l.addAll(temp);
+				}
+				break;
+			case "removeAll":
+				if (tokens.length == 2) {
+					String[] c = tokens[1].split(",");
+					int[] temp = new int[c.length];
+					for (int i = 0; i < c.length; i++) {
+						temp[i] = Integer.parseInt(c[i]);
 					}
+					l.removeAll(temp);
+				}
+				break;
+			case "count":
+				if (tokens.length == 2) {
+					System.out.println(l.count(Integer.parseInt(tokens[1])));
+				}
+				break;
+			case "subList":
+				if (tokens.length != 2) {
 					break;
-				case "indexOf":
-					if(tokens.length==2) {
-						System.out.println(l.indexOf(Integer.parseInt(tokens[1])));
+				}
+				String[] d = tokens[1].split(",");
+				try {
+					System.out.println(l.subList(Integer.parseInt(d[0]), Integer.parseInt(d[1])));
+				} catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println("ArrayIndexOutOfBoundsException");
+				}
+				break;
+			case "equals":
+				if (tokens.length == 2) {
+					String[] lt = tokens[1].split(",");
+					Solution l2 = new Solution();
+					for (int k = 0; k < lt.length; k++) {
+						l2.add(Integer.parseInt(lt[k]));
 					}
-					break;
-				case "addAll":
-					if(tokens.length==2) {
-						String[] b = tokens[1].split(",");
-						int[] temp= new int[b.length];
-						for(int i=0;i<b.length;i++) {
-							temp[i]=Integer.parseInt(b[i]);
-						}
-						l.addAll(temp);
-					}
-					break;
-				case "removeAll":
-					if(tokens.length==2) {
-						String[] c = tokens[1].split(",");
-						int[] temp= new int[c.length];
-						for(int i=0;i<c.length;i++) {
-							temp[i]=Integer.parseInt(c[i]);
-						}
-						l.removeAll(temp);
-					}
-					break;
-				case "count":
-					if(tokens.length==2) {
-						System.out.println(l.count(Integer.parseInt(tokens[1])));
-					}
-					break;
-				case "subList":
-					if(tokens.length!=2) {
-						break;
-					}
-					String[] d = tokens[1].split(",");
-					System.out.println(l.subList(Integer.parseInt(d[0]),Integer.parseInt(d[1])));
-					break;
-				case "equals":
-					if (tokens.length == 2) {
-                    	String[] lt = tokens[1].split(",");
-                    	Solution l2 = new Solution();
-                    	for (int k = 0; k < lt.length; k++) {
-                        	l2.add(Integer.parseInt(lt[k]));
-                    	}
-                    	System.out.println(l.equals(l2));
-                	}
-                	break;
-                case "clear":
-                	l.clear();
-                	break;
-                default :
-                	break;
+					System.out.println(l.equals(l2));
+				}
+				break;
+			case "clear":
+				l.clear();
+				break;
+			default :
+				break;
 			}
 		}
 	}
