663e05e99d1b4be439e44095704ecabb55253dca
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-14 23:19:58 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m17/Shoppingcart/Solution.java
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -125,13 +125,13 @@ class ShoppingCart {
 					System.out.println(cart[i].getProductName() + " " + cart[i].getQuantity() + " " + catalog[j].getUnitPrice());
 			}
 		}
-		System.out.println("totalAmount:" + totalAmount());
+		System.out.println("totalAmount: " + totalAmount());
 		System.out.println("Total:" + totalAmount());
 		float discount = totalAmount() * couponcode;
 		System.out.println("Disc%:" + discount);
 		float finalAmount = totalAmount() - discount;
 		System.out.println("Tax:" + (finalAmount * 15 / 100));
-		System.out.println("Payable amount:" + getPayableAmount());
+		System.out.println("Payable amount: " + getPayableAmount());
 	}
 
 }
