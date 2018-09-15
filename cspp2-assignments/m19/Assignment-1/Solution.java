d6dc4502105dbb7d35daef31bfa0ee47b69294c2
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-15 13:11:45 +0530

    "check_style_score -> 2 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -180,7 +180,7 @@ public final class Solution {
                     System.out.println("Invalid penalty for "+tokens1[0]);
                     n=1;
                 } 
-            else if (tokens1.length<4) {
+            else if (tokens1.length<5&&tokens1[0]==null) {
                     System.out.println("Error! Malformed question");
                     n=1;
 
