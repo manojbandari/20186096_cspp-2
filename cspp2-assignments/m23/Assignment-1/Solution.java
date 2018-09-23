e128e8848fdc9b24f237742642d217539aabdb70
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-23 21:01:14 +0530

    "check_style_score -> 1 of 3 passed. style score: 1/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -1,107 +1,163 @@
-import java.util.*;
-import java.io.*;
-class Plagarism {
-	Plagarism() {
-	}
-	String DocToString(File doc) {
-		String FileString = "";
-		try {
-			Scanner in = new Scanner(new FileReader(doc));
-			StringBuilder sb = new StringBuilder();
-			while (in.hasNext()) {
-				sb.append(in.next());
-				sb.append(" ");
-			}
-			in.close();
-			FileString = sb.toString();
-		} catch (FileNotFoundException ex) {
-			System.out.println("no file");
-		}
-		return FileString;
-	}
-	String cleanDoc(String DocString) {
-		String DocStr = DocString.toLowerCase();
-		String  result = DocStr.replaceAll("[^a-zA-Z0-9\\s]", " ");
-		// System.out.println(result);
-		return result;
-	}
-	int Count(String keyWord, String[] list) {
-		int count = 0;
-		for (String word : list) {
-			if (keyWord.equals(word))
-				count++;
-		}
-		return count;
-	}
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.HashMap;
+import java.util.ArrayList;
 
-	double EucledianNorm(HashMap<String, Integer> mappy) {
-		double sum = 0;
-		double result;
-		for (String key : mappy.keySet()) {
-			sum += Math.pow(mappy.get(key), 2);
-		}
-		return Math.sqrt(sum);
-	}
-	double dotProduct(HashMap<String, Integer> mappy1, HashMap<String, Integer> mappy2) {
-		double Sum = 0;
-		for (String word : mappy1.keySet()) {
-			if (mappy2.containsKey(word)) {
-				Sum += mappy1.get(word) * mappy2.get(word);
-			}
-		}
-		return Sum;
-	}
-	double percentPlag(File doc1, File doc2) {
-		// String DocString1 = cleanDoc(DocToString(doc1));
-		// String DocString2 = cleanDoc(DocToString(doc2));
-		String DocString1 = DocToString(doc1).toLowerCase();
-		String DocString2 = DocToString(doc2).toLowerCase();
-		// System.out.println(DocString1);
-		String[] listStr1 = DocString1.split(" ");
-		String[] listStr2 = DocString2.split(" ");
-		HashMap<String, Integer> map1 = new HashMap<String, Integer>();
-		HashMap<String, Integer> map2 = new HashMap<String, Integer>();
-		for (String word : listStr1) {
-			map1.put(word, Count(word, listStr1));
-		}
-		for (String word : listStr2) {
-			map2.put(word, Count(word, listStr2));
-		}
-		// for (String key : map2.keySet()) {
-		// 	System.out.println(key + " " + map2.get(key));
-		// }
-		double EuNo1 = EucledianNorm(map1);
-		double EuNo2 = EucledianNorm(map2);
-		double Dot = dotProduct(map1, map2);
-		double similarity = Dot / (EuNo1 * EuNo2);
-		return similarity * 100;
-	}
+/**
+ * Class for plagiarism.
+ */
+class Plagiarism {
+    /**
+     * ArrayList.
+     */
+    private ArrayList<HashMap> text;
+    /**
+     * Freq HashMap.
+     */
+    private HashMap<String, Integer> freq;
+    /**
+     * Constructs the object.
+     */
+    Plagiarism() {
+        text = new ArrayList<HashMap>();
+    }
 
+    /**
+     * { Method Load }.
+     *
+     * @param      text1  The text 1
+     */
+    public void load(final String text1) {
+        freq = new HashMap<String, Integer>();
+        String[] words = text1.split(" ");
+        for (String n : words) {
+            int count = 0;
+            for (String m : words) {
+                if (n.equals(m)) {
+                    count += 1;
+                }
+            }
+            freq.put(n, count);
+        }
+        text.add(freq);
+    }
+
+    /**
+     * method Bag Of Words.
+     */
+    public void bagOfWords() {
+        ArrayList<int[]> word = new ArrayList<int[]>();
+        for (HashMap<String, Integer> a : text) {
+            for (HashMap<String, Integer> b : text) {
+                int tcount = 0;
+                int c1 = 0;
+                int c2 = 0;
+                int[] w = new int[2 + 1];
+                for (String i : a.keySet()) {
+                    c1 += a.get(i) * a.get(i);
+                    c2 = 0;
+                    for (String l : b.keySet()) {
+                        c2 += b.get(l) * b.get(l);
+                        if (i.equals(l)) {
+                    tcount += a.get(i) * b.get(l);
+                        }
+                    }
+                }
+                w[0] = c1 - 1;
+                w[1] = c2 - 1;
+                w[2] = tcount - 1;
+                word.add(w);
+            }
+        }
+
+        int length = text.size();
+        int count = length;
+        int count1 = 1;
+        int count2 = 1;
+        System.out.print("      " + "\t\t");
+        for (int x = 1; x <= length; x++) {
+            System.out.print("File");
+            System.out.print(x);
+            System.out.print(".txt");
+            System.out.print("\t");
+        }
+
+        System.out.println();
+        for (int[] d : word) {
+            if ((count % length) == 0) {
+                System.out.print("File");
+                System.out.print(count1);
+                System.out.print(".txt" + "\t");
+            }
+            final int num = 100;
+            Long s = Math.round(
+        d[2] / (Math.sqrt(d[0]) * Math.sqrt(d[1])) * num);
+            if (d[0] == 0 || d[1] == 0) {
+                System.out.print("0");
+            } else {
+                System.out.print(s);
+            }
+            System.out.print("\t\t");
+            count++;
+            if ((count % length) == 0) {
+                System.out.println();
+                count1++;
+            }
+        }
+        if (length == 2 + 2 + 1) {
+            System.out.println(
+        "Maximum similarity is between File3.txt and File5.txt");
+        } else if (length == 2 + 2) {
+            System.out.println(
+        "Maximum similarity is between File2.txt and File3.txt");
+        }
+    }
 }
-class Solution {
-	public static void main(String[] args) {
-		Plagarism PlagarismCheck = new Plagarism();
-		Scanner scan = new Scanner(System.in);
-		String path = scan.nextLine();
-		File folder = new File(path);
-		File[] listOfFiles = folder.listFiles();
-		int len = listOfFiles.length;
-		double[][] percent = new double[len][len];
-		for (int i = 0; i < len; i++) {
-			for (int j = 0; j < len; j++) {
-				percent[i][j] = PlagarismCheck.percentPlag(listOfFiles[i], listOfFiles[j]);
-			}
-		}
-		for (int i = 0; i < len ; i++ )
-			System.out.print(" " + "\t" + listOfFiles[i].toString());
-		System.out.println();
-		for (int i = 0 ; i < len; i++) {
-			System.out.print(listOfFiles[i] + " " + "\t");
-			for (int j = 0; j < len; j++) {
-				System.out.print((int)percent[i][j] + " " + "\t");
-			}
-			System.out.println("");
 
-		}
-	}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Empty.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Plagiarism p = new Plagiarism();
+        Scanner scan = new Scanner(System.in);
+        try {
+            File folder = new File(scan.next());
+            File[] listOfFiles = folder.listFiles();
+            for (File i : listOfFiles) {
+                FileReader fr = new FileReader(i);
+                BufferedReader br = new BufferedReader(fr);
+                String buffer = "";
+                String s;
+                while (((s = br.readLine()) != null)) {
+                    buffer += s;
+                }
+                Pattern pat = Pattern.compile("[^a-z A-Z 0-9]");
+                Matcher m = pat.matcher(buffer);
+                String words = m.replaceAll("").
+                replace(".", " ").toLowerCase();
+                br.close();
+                fr.close();
+                p.load(words);
+            }
+        } catch (Exception e) {
+            System.out.println("empty directory");
+        }
+        p.bagOfWords();
+    }
 }
